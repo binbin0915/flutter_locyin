b7e7e1f9b91aa74a71c5ea9f513716432aadbd3a
geekadpt <geekadpt@163.com>
2021-08-27 23:37:28 +0800

    高德定位、高德地图

---
+++ lib/widgets/locator.dart
@@ -0,0 +1,200 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:amap_flutter_location/amap_flutter_location.dart';
+import 'package:amap_flutter_location/amap_location_option.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+
+class LocatorWidget extends StatefulWidget {
+  final Function onPressed;
+
+  const LocatorWidget({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  _LocatorWidgetState createState() => new _LocatorWidgetState();
+}
+
+class _LocatorWidgetState extends State<LocatorWidget> {
+  Map<String, Object>? _locationResult;
+
+  late StreamSubscription<Map<String, Object>> _locationListener;
+
+  AMapFlutterLocation _locationPlugin = new AMapFlutterLocation();
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// 动态申请定位权限
+    requestPermission();
+
+    ///设置Android和iOS的apiKey<br>
+    ///
+    /// 定位Flutter插件提供了单独的设置ApiKey的接口，
+    /// 使用接口的优先级高于通过Native配置ApiKey的优先级（通过Api接口配置后，通过Native配置文件设置的key将不生效），
+    /// 使用时可根据实际情况决定使用哪种方式
+    ///
+    ///key的申请请参考高德开放平台官网说明<br>
+    ///
+    ///Android: https://lbs.amap.com/api/android-location-sdk/guide/create-project/get-key
+    ///
+    ///iOS: https://lbs.amap.com/api/ios-location-sdk/guide/create-project/get-key
+    //AMapFlutterLocation.setApiKey("617d99f861823395679068efa584d09a", "");
+
+    ///iOS 获取native精度类型
+    if (Platform.isIOS) {
+      requestAccuracyAuthorization();
+    }
+
+    ///注册定位结果监听
+    _locationListener = _locationPlugin
+        .onLocationChanged()
+        .listen((Map<String, Object> result) {
+      setState(() {
+        print("获取到定位信息：");
+        print(result);
+        _locationResult = result;
+      });
+    });
+    _startLocation();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    ///移除定位监听
+    _locationListener.cancel();
+
+    ///销毁定位
+    _locationPlugin.destroy();
+  }
+
+  ///设置定位参数
+  void _setLocationOption() {
+    AMapLocationOption locationOption = new AMapLocationOption();
+
+    ///是否单次定位
+    locationOption.onceLocation = true;
+
+    ///是否需要返回逆地理信息
+    locationOption.needAddress = true;
+
+    ///逆地理信息的语言类型
+    locationOption.geoLanguage = GeoLanguage.DEFAULT;
+
+    locationOption.desiredLocationAccuracyAuthorizationMode =
+        AMapLocationAccuracyAuthorizationMode.ReduceAccuracy;
+
+    locationOption.fullAccuracyPurposeKey = "AMapLocationScene";
+
+    ///设置Android端连续定位的定位间隔
+    locationOption.locationInterval = 2000;
+
+    ///设置Android端的定位模式<br>
+    ///可选值：<br>
+    ///<li>[AMapLocationMode.Battery_Saving]</li>
+    ///<li>[AMapLocationMode.Device_Sensors]</li>
+    ///<li>[AMapLocationMode.Hight_Accuracy]</li>
+    locationOption.locationMode = AMapLocationMode.Hight_Accuracy;
+
+    ///设置iOS端的定位最小更新距离<br>
+    locationOption.distanceFilter = -1;
+
+    ///设置iOS端期望的定位精度
+    /// 可选值：<br>
+    /// <li>[DesiredAccuracy.Best] 最高精度</li>
+    /// <li>[DesiredAccuracy.BestForNavigation] 适用于导航场景的高精度 </li>
+    /// <li>[DesiredAccuracy.NearestTenMeters] 10米 </li>
+    /// <li>[DesiredAccuracy.Kilometer] 1000米</li>
+    /// <li>[DesiredAccuracy.ThreeKilometers] 3000米</li>
+    locationOption.desiredAccuracy = DesiredAccuracy.Best;
+
+    ///设置iOS端是否允许系统暂停定位
+    locationOption.pausesLocationUpdatesAutomatically = false;
+
+    ///将定位参数设置给定位插件
+    _locationPlugin.setLocationOption(locationOption);
+  }
+
+  ///开始定位
+  void _startLocation() {
+    _setLocationOption();
+    _locationPlugin.startLocation();
+  }
+
+  ///停止定位
+  void _stopLocation() {
+    if (null != _locationPlugin) {
+      _locationPlugin.stopLocation();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    /*if (_locationResult != null) {
+      _locationResult!.forEach((key, value) {
+        print("key:$key,value:$value");
+      });
+    }*/
+    return Container(
+      child:Material(
+          shape: CircleBorder(
+              side: BorderSide(
+                  color: Colors.green,
+                  width: 2,
+                  style: BorderStyle.solid)),
+          child: IconButton(
+            icon: Icon(Icons.location_on_outlined),
+            onPressed: () {  widget.onPressed(_locationResult); },
+          ),
+      )
+    );
+  }
+
+  ///获取iOS native的accuracyAuthorization类型
+  void requestAccuracyAuthorization() async {
+    AMapAccuracyAuthorization currentAccuracyAuthorization =
+    await _locationPlugin.getSystemAccuracyAuthorization();
+    if (currentAccuracyAuthorization ==
+        AMapAccuracyAuthorization.AMapAccuracyAuthorizationFullAccuracy) {
+      print("精确定位类型");
+    } else if (currentAccuracyAuthorization ==
+        AMapAccuracyAuthorization.AMapAccuracyAuthorizationReducedAccuracy) {
+      print("模糊定位类型");
+    } else {
+      print("未知定位类型");
+    }
+  }
+
+  /// 动态申请定位权限
+  void requestPermission() async {
+    // 申请权限
+    bool hasLocationPermission = await requestLocationPermission();
+    if (hasLocationPermission) {
+      print("定位权限申请通过");
+    } else {
+      print("定位权限申请不通过");
+    }
+  }
+
+  /// 申请定位权限
+  /// 授予定位权限返回true， 否则返回false
+  Future<bool> requestLocationPermission() async {
+    //获取当前的权限
+    var status = await Permission.location.status;
+    if (status == PermissionStatus.granted) {
+      //已经授权
+      return true;
+    } else {
+      //未授权则发起一次申请
+      status = await Permission.location.request();
+      if (status == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
